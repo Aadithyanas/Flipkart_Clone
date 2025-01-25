78ae873bfda26d3ce3b5a83c91b420aab0553f17
Aadithyan <rak34316@gmail.com>
2025-01-25 13:37:03 +0530

    update

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Connection = async (username, password) => {
     // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = `mongodb://${username}:${password}@ecommerce-shard-00-00.fdvft.mongodb.net:27017,ecommerce-shard-00-01.fdvft.mongodb.net:27017,ecommerce-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-ilaj5d-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    const URL = 'mongodb+srv://adithyanas2694:caewQ8BhWt42ioY4@ecommerceWeb.vk48r.mongodb.net/'
+    const URL = 'mongodb+srv://Adithyan:Aadithyan@123@filpkart.emdqt.mongodb.net/?retryWrites=true&w=majority&appName=filpkart'
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
