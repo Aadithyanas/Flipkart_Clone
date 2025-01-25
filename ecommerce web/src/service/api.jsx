4e2720e82123c4c3135904da39a793e7de73e5e2
Aadithyan <rak34316@gmail.com>
2025-01-25 15:45:10 +0530

    update

--- ecommerce web/src/service/api.jsx
+++ ecommerce web/src/service/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = 'https://flipkart-clone-x3a3.onrender.com/';
 
 export const authenticateLogin = async (user) => {
     try {
